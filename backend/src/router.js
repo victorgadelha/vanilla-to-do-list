f94b3dadbd73586f2b8744c45e0a7b342675aee9
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-01-25 20:06:53 -0300

    feat: criando conexão com o banco de dados

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/tasks', (req, res) => {
-    res.status(200).send('O router está funcionando!');
-});
+//router.get('/tasks', (req, res) => {
+//res.status(200).send('O router está funcionando!');
+//});
 
 module.exports = router;
