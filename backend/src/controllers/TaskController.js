5a1662be444a341c00a5d946b4c64b281db6ad25
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-01-25 20:56:43 -0300

    fix: consertando método getAll na rota tasks

--- backend/src/controllers/TaskController.js
+++ backend/src/controllers/TaskController.js
@@ -1,4 +1,6 @@
-const getAll = () => {};
+const getAll = (req, res) => {
+    return res.status(200).json({ message: 'controller está tudo certo.' });
+};
 
 module.exports = {
     getAll,
--- backend/src/router.js
+++ backend/src/router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const TaskController = require('./TaskController');
+const TaskController = require('../src/controllers/TaskController');
 
 const router = express.Router();
 
