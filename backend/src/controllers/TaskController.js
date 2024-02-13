6e21a428f10fa048a164a5c750b51c7a9a5fa2b3
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-02-13 19:47:24 -0300

    refactor: mudando nome do arquivo das models das tasks

--- backend/src/controllers/TaskController.js
+++ backend/src/controllers/TaskController.js
@@ -1,4 +1,4 @@
-const tasksModel = require('../models/Task');
+const tasksModel = require('../models/taskModel');
 const getAll = async (req, res) => {
     const tasks = await tasksModel.getAll();
     return res.status(200).json(tasks);
