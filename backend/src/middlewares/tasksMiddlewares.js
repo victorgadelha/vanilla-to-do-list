f7aca969ec4bd6542d4a0cb07f610b0fdf4d1289
João Victor Gadelha <joaovictor_gadelha@live.com>
2024-02-19 20:34:49 -0300

    feat: criando middleware para validar status e inserindo na rota

--- backend/src/middlewares/tasksMiddlewares.js
+++ backend/src/middlewares/tasksMiddlewares.js
@@ -1,4 +1,4 @@
-const validateBody = (req, res, next) => {
+const validateFieldTitle = (req, res, next) => {
     const { body } = req;
 
     if (body.title === undefined) {
@@ -16,6 +16,25 @@ const validateBody = (req, res, next) => {
     next();
 };
 
+const validateFieldStatus = (req, res, next) => {
+    const { body } = req;
+
+    if (body.status === undefined) {
+        return res
+            .status(400)
+            .json({ message: 'the field "status" is required' });
+    }
+
+    if (body.status === '') {
+        return res
+            .status(400)
+            .json({ message: 'the field "status" cannot be empty' });
+    }
+
+    next();
+};
+
 module.exports = {
-    validateBody,
+    validateFieldTitle,
+    validateFieldStatus,
 };
--- backend/src/router.js
+++ backend/src/router.js
@@ -5,8 +5,17 @@ const TasksController = require('../src/controllers/TaskController');
 const tasksMiddleware = require('./middlewares/tasksMiddlewares');
 
 router.get('/tasks', TasksController.getAll);
-router.post('/tasks', tasksMiddleware.validateBody, TasksController.createTask);
+router.post(
+    '/tasks',
+    tasksMiddleware.validateFieldTitle,
+    TasksController.createTask
+);
 router.delete('/tasks/:id', TasksController.deleteTask);
-router.put('/tasks/:id', TasksController.updateTask);
+router.put(
+    '/tasks/:id',
+    tasksMiddleware.validateFieldTitle,
+    tasksMiddleware.validateFieldStatus,
+    TasksController.updateTask
+);
 
 module.exports = router;
